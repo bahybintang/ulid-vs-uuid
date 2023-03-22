745daac22c4e2006c7c59001002e55736f6711ce
Muhammad Bintang Bahy <bahybintang@gmail.com>
2023-03-22 20:30:46 +0700

    fix bug

--- tests/find_ulid_test.sql
+++ tests/find_ulid_test.sql
@@ -1,3 +1,3 @@
 \c rpl
 \timing on
-SELECT find_random_ulid(ARRAY(SELECT uuid FROM uuid_testing TABLESAMPLE BERNOULLI(100000/(SELECT COUNT(*) FROM uuid_testing))));
+SELECT find_random_ulid(ARRAY(SELECT ulid FROM ulid_testing TABLESAMPLE BERNOULLI(100000/(SELECT COUNT(*) FROM ulid_testing))));
